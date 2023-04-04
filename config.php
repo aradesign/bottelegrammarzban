4fc71c2c612bc4d5dc82d6aa27ad2ecbce689d97
MahdiGholipour <mahdigholipour@outlook.com>
2023-04-04 21:37:03 +0330

    Botfull (#1)
    * Add Feature
    * fixed  bgu

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php 
+# id telegram @gholipour3
+$url_panel="https://site.com:8080";/*آدرس پنل */
+$username_panel = "admin";#نام کاربری پنل مرزبان را وارد کنید
+$password_panel = "admin"; # رمز عبور پنل مرزبان را وارد نمایید
+$token = "TOKEN";# توکن ربات
+$time = "1";# زمان اکانت تست بر اساس ساعت
+$val = "100"; #حجم اکانت تست بر اساس مگابایت
