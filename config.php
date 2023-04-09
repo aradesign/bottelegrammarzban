fc5c4410e025fa8bfa3df26e699a3e62f3605181
MahdiGholipour <mahdigholipour@outlook.com>
2023-04-10 00:51:59 +0330

    add database
    تغییر دیتابیس به sql

--- config.php
+++ config.php
@@ -1,9 +1,17 @@
-<?php 
-# id telegram @gholipour3
-$url_panel="https://site.com:8080";/*آدرس پنل */
-$username_panel = "admin";#نام کاربری پنل مرزبان را وارد کنید
-$password_panel = "admin"; # رمز عبور پنل مرزبان را وارد نمایید
-$token = "TOKEN";# توکن ربات
-$time = "1";# زمان اکانت تست بر اساس ساعت
-$val = "100"; #حجم اکانت تست بر اساس مگابایت
-$adminidnumbeer = "1111111";     #آیدی عددی ادمین
+<?php
+//-----------------------------database-------------------------------
+$dbname = "databasename"; //  نام دیتابیس
+$username = "databasename"; // نام کاربری دیتابیس
+$password = 'password'; // رمز عبور دیتابیس
+$connect = mysqli_connect("localhost", $username, $password, $dbname);
+//-----------------------------ifno-------------------------------
+$token = "TOKEN";  //  توکن ربات خود
+$adminnumber ="111111";//   آیدی عددی ادمین بطور پیشفرض
+//-----------------------------ifno panel-------------------------------
+$url_panel = "https://site.com:8080";//آدرس پنل مرزبان
+$username_panel = "admin";// نام کاربری پنل مرزبان
+$password_panel ="admin"; // رمز عبور پنل مرزبان
+$time = "1";// زمان پایان یوزر تست براساس ساعت
+$val = "100";// حجم اکانت تست براساس مگابایت
+$limit_usertest ='1';// محدودیت ساخت اکانت تست  بطور پیشفرض
+
