055d565bd89924fd8df137890fabb378268720dc
MahdiGholipour <mahdigholipour@outlook.com>
2023-04-10 00:52:53 +0330

    create table

---
+++ table.php
@@ -0,0 +1,20 @@
+<?php
+include ('config.php');
+//-----------------------------------------------------------------
+$connect->query("CREATE TABLE user (
+id varchar(30) PRIMARY KEY,
+limit_usertest int(100) NOT NULL ,
+step varchar(5000) NOT NULL)");
+//-----------------------------------------------------------------
+$connect->query("CREATE TABLE admin (
+id_admin int(100) NOT NULL )");
+//-----------------------------------------------------------------
+$connect->query("CREATE TABLE channels (
+Channel_lock varchar(200) NOT NULL,
+link varchar(200) NOT NULL )");
+//-----------------------------------------------------------------
+$connect->query("INSERT INTO admin (id_admin) VALUES ('$adminnumber')");
+$connect->query("INSERT INTO channels (link,
+Channel_lock) VALUES ('test','off')");
+
+echo "<b>Table Created!</b>";
