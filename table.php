6c3a52411f0c0bf5b020dd01ec8f7b63c1ba0fd8
MahdiGholipour <mahdigholipour@outlook.com>
2023-04-14 23:11:56 +0330

    Update table.php

--- table.php
+++ table.php
@@ -1,20 +1,82 @@
 <?php
-include ('config.php');
+include('config.php');
 //-----------------------------------------------------------------
-$connect->query("CREATE TABLE user (
-id varchar(30) PRIMARY KEY,
-limit_usertest int(100) NOT NULL ,
-step varchar(5000) NOT NULL)");
+try {
+    $result = $connect->query("SHOW TABLES LIKE 'user'");
+    $table_exists = ($result->num_rows > 0);
+
+    if (!$table_exists) {
+        $connect->query("CREATE TABLE user (
+        id varchar(30) PRIMARY KEY,
+        limit_usertest int(100) NOT NULL,
+        Processing_value varchar(100000) NOT NULL,
+        step varchar(5000) NOT NULL)");
+        echo "table user✅</br>";
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+
 //-----------------------------------------------------------------
-$connect->query("CREATE TABLE admin (
-id_admin varchar(5000) NOT NULL )");
+try {
+    $result = $connect->query("SHOW TABLES LIKE 'admin'");
+    $table_exists = ($result->num_rows > 0);
+    if ($table_exists) {
+        $id_admin = mysqli_query($connect, "SELECT * FROM admin");
+        while ($row = mysqli_fetch_assoc($id_admin)) {
+            $admin_ids[] = $row['id_admin'];
+        }
+        if (!in_array($adminnumber, $admin_ids)) {
+            $connect->query("INSERT INTO admin (id_admin) VALUES ('$adminnumber')");
+            echo "table admin update✅</br>";
+        }
+    } else {
+        $connect->query("CREATE TABLE admin (
+        id_admin varchar(5000) NOT NULL)");
+        $connect->query("INSERT INTO admin (id_admin) VALUES ('$adminnumber')");
+        echo "table admin ✅</br>";
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
 //-----------------------------------------------------------------
-$connect->query("CREATE TABLE channels (
+try {
+
+    $result = $connect->query("SHOW TABLES LIKE 'channels'");
+    $table_exists = ($result->num_rows > 0);
+
+    if ($table_exists) {
+        $channels = mysqli_fetch_assoc(mysqli_query($connect, "SELECT * FROM channels"));
+        if (!isset($channels['link'])) {
+            $connect->query("INSERT INTO channels (link,Channel_lock) VALUES ('test','off')");
+            echo "table channels updated ✅ </br>";
+        }
+    } else {
+        $connect->query("CREATE TABLE channels (
 Channel_lock varchar(200) NOT NULL,
 link varchar(200) NOT NULL )");
-//-----------------------------------------------------------------
-$connect->query("INSERT INTO admin (id_admin) VALUES ('$adminnumber')");
-$connect->query("INSERT INTO channels (link,
-Channel_lock) VALUES ('test','off')");
+        $connect->query("INSERT INTO channels (link,Channel_lock) VALUES ('test','off')");
+        echo "table channels ✅ </br>";
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+//--------------------------------------------------------------
+try {
 
-echo "<b>Table Created!</b>";
+    $result = $connect->query("SHOW TABLES LIKE 'marzban_panel'");
+    $table_exists = ($result->num_rows > 0);
+
+    if (!$table_exists) {
+        $connect->query("CREATE TABLE marzban_panel (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name_panel varchar(2000) NOT NULL,
+        url_panel varchar(2000) NOT NULL,
+        username_panel varchar(200) NOT NULL,
+        password_panel varchar(200) NOT NULL )");
+        echo "table marzban_panel ✅ </br>";
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+//-----------------------------------------------------------------
