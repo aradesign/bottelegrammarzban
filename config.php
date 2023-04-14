ab555c410dbbcfbb9bbeeba79de62590aba5d049
MahdiGholipour <mahdigholipour@outlook.com>
2023-04-14 23:10:16 +0330

    Update config.php

--- config.php
+++ config.php
@@ -8,9 +8,6 @@ $connect = mysqli_connect("localhost", $username, $password, $dbname);
 $token = "TOKEN";  //  توکن ربات خود
 $adminnumber ="111111";//   آیدی عددی ادمین بطور پیشفرض
 //-----------------------------ifno panel-------------------------------
-$url_panel = "https://site.com:8080";//آدرس پنل مرزبان
-$username_panel = "admin";// نام کاربری پنل مرزبان
-$password_panel ="admin"; // رمز عبور پنل مرزبان
 $time = "1";// زمان پایان یوزر تست براساس ساعت
 $val = "100";// حجم اکانت تست براساس مگابایت
 $limit_usertest ='1';// محدودیت ساخت اکانت تست  بطور پیشفرض
