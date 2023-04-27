a33bdea34d9d0f7019f783392a23cb7cdd8e8689
MahdiGholipour <mahdigholipour@outlook.com>
2023-04-27 15:38:33 +0330

    Update table.php

--- table.php
+++ table.php
@@ -8,13 +8,37 @@ try {
 
     if (!$table_exists) {
         $connect->query("CREATE TABLE user (
-        id varchar(1000) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        id varchar(500)  PRIMARY KEY,
         limit_usertest int(100) NOT NULL,
-        Processing_value varchar(100000) NOT NULL,
+        roll_Status bool NOT NULL,
+        Processing_value varchar(1000) NOT NULL,
         step varchar(5000) NOT NULL,
         description_blocking varchar(5000) NOT NULL,
         User_Status varchar(500) NOT NULL)");
         echo "table user✅</br>";
+    } else {
+        $Check_filde = $connect->query("SHOW COLUMNS FROM user LIKE 'Processing_value'");
+        if (mysqli_num_rows($Check_filde) != 1) {
+            $connect->query("ALTER TABLE user ADD Processing_value VARCHAR(1000)");
+            $connect->query("UPDATE user SET Processing_value = 'none'");
+            echo "The Processing_Value field was added ✅";
+        }
+        $Check_filde = $connect->query("SHOW COLUMNS FROM user LIKE 'roll_Status'");
+        if (mysqli_num_rows($Check_filde) != 1) {
+            $connect->query("ALTER TABLE user ADD roll_Status bool");
+            $connect->query("UPDATE user SET roll_Status = false");
+            echo "The roll_Status field was added ✅";
+        }
+        $Check_filde = $connect->query("SHOW COLUMNS FROM user LIKE 'description_blocking'");
+        if (mysqli_num_rows($Check_filde) != 1) {
+            $connect->query("ALTER TABLE user ADD description_blocking VARCHAR(5000)");
+            echo "The description_blocking field was added ✅";
+        }
+        $Check_filde = $connect->query("SHOW COLUMNS FROM user LIKE 'User_Status'");
+        if (mysqli_num_rows($Check_filde) != 1) {
+            $connect->query("ALTER TABLE user ADD User_Status VARCHAR(500)");
+            echo "The User_Status field was added ✅";
+        }
     }
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
@@ -30,23 +54,24 @@ try {
         name_os varchar(500) NOT NULL,
         Media_os varchar(5000) NOT NULL,
         type_Media_os varchar(500) NOT NULL,
-        Description_os TEXT(10000) NOT NULL)");
+        Description_os TEXT NOT NULL)");
         echo "table help✅</br>";
     }
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
+
 //-----------------------------------------------------------------
 try {
     $result = $connect->query("SHOW TABLES LIKE 'textbot'");
     $table_exists = ($result->num_rows > 0);
-      $text_info = "
+    $text_info = "
       نام کاربری خود را ارسال نمایید
             
     ⚠️ نام کاربری باید بدون کاراکترهای اضافه مانند @ ، فاصله ، خط تیره باشد. 
     ⚠️ نام کاربری باید انگلیسی باشد
       ";
-      $text_usertest = "
+    $text_usertest = "
       👤برای ساخت اشتراک تست یک نام کاربری انگلیسی ارسال نمایید.
     
     ⚠️ نام کاربری باید دارای شرایط زیر باشد
@@ -57,21 +82,88 @@ try {
     
     🛑 در صورت رعایت نکردن موارد بالا با خطا مواجه خواهید شد
       ";
-    $helpt = "📚  آموزش";
-    $supportt = "☎️ پشتیبانی";
-    $bot_off = " ❌ ربات خاموش است ، لطفا دقایقی دیگر مراجعه کنید";
+    $support_dec = "
+    پیام خود را ارسال کنید کنید :
+        ⚠️ برای دریافت پیام حتما باید فوروارد حساب کاربری تان باز باشد تا پاسخ ادمین را دریافت کنید.
+    ";
+    $text_roll = "
+    ♨️ قوانین استفاده از خدمات ما
+
+1 - به اطلاعیه هایی که داخل کانال گذاشته می شود حتما توجه کنید.
+2- در صورتی که اطلاعیه ای در مورد قطعی در کانال گذاشته نشده به اکانت پشتیبانی پیام دهید
+3- سرویس ها را از طریق پیامک ارسال نکنید برای ارسال پیامک می توانید از طریق ایمیل ارسال کنید.
+    ";
+    $text_dec_fq = " 
+    💡 سوالات متداول
+
+
+❓ فیلترشکن شما آیپی ثابته ؟ میتونم برای صرافی های ارز دیجیتال استفاده کنم؟
+
+      • به دلیل وضعیت نت و محدودیت های کشور سرویس ما مناسب ترید نیست و فقط لوکیشن‌ ثابته.
+
+
+❓ اگه قبل از منقضی شدن اکانت، تمدیدش کنم روزهای باقی مانده میسوزد؟
+
+      • خیر، روزهای باقیمونده اکانت موقع تمدید حساب میشن و اگه مثلا 5 روز قبل از منقضی شدن اکانت 1 ماهه خودتون اون رو تمدید کنید 5 روز باقیمونده + 30 روز تمدید میشه.
+
+
+❓ اگه به یک اکانت بیشتر از حد مجاز متصل شیم چه اتفاقی میافته؟
+
+      • در این صورت حجم سرویس شما زود تمام خواهد شد.
+
+
+❓ فیلترشکن شما از چه نوعیه؟
+
+      • فیلترشکن های ما v2ray است و پروتکل‌های مختلفی رو ساپورت میکنیم تا حتی تو دورانی که اینترنت اختلال داره بدون مشکل و افت سرعت بتونید از سرویستون استفاده کنید.
+
+
+❓ فیلترشکن از کدوم کشور است؟
+
+      • سرور فیلترشکن ما از کشور  آلمان است
+
+
+❓ چطور باید از این فیلترشکن استفاده کنم؟
+
+      • برای آموزش استفاده از برنامه، روی دکمه «📚 آموزش» بزنید.
+
+
+❓ فیلترشکن وصل نمیشه، چیکار کنم؟
+
+      • به همراه یک عکس از پیغام خطایی که میگیرید به پشتیبانی مراجعه کنید.
+
+
+❓ فیلترشکن شما تضمینی هست که همیشه مواقع متصل بشه؟
+
+      • به دلیل قابل پیش‌بینی نبودن وضعیت نت کشور، امکان دادن تضمین نیست فقط می‌تونیم تضمین کنیم که تمام تلاشمون رو برای ارائه سرویس هر چه بهتر انجام بدیم.
+
+
+❓ امکان بازگشت وجه دارید؟
+
+      • امکان بازگشت وجه در صورت حل نشدن مشکل از سمت ما وجود دارد.
+
+
+
+💡 در صورتی که جواب سوالتون رو نگرفتید میتونید به «پشتیبانی» مراجعه کنید.";
     if (!$table_exists) {
         $connect->query("CREATE TABLE textbot (
-        text_info varchar(100000) NOT NULL,
-        text_dec_info varchar(100000) NOT NULL,
-        text_usertest varchar(100000) NOT NULL,
-        text_dec_usertest varchar(100000) NOT NULL,
-        text_help varchar(1000) NOT NULL,
-        text_support varchar(1000) NOT NULL,
-        text_dec_bot_off varchar(7000) NOT NULL,
-        text_start varchar(100000) NOT NULL)");
+        id_text varchar(2000) NOT NULL,
+        text TEXT NOT NULL)");
         echo "table textbot✅</br>";
-        $connect->query("INSERT INTO textbot (text_start,text_info,text_usertest,text_dec_info,text_dec_usertest,text_help,text_support,text_dec_bot_off ) VALUES ('سلام خوش آمدید','📊  اطلاعات سرویس','🔑 اکانت تست','$text_info','$text_usertest','$helpt','$supportt','$bot_off')");
+        $connect->query("INSERT INTO textbot (id_text,text) VALUES ('text_start','سلام خوش آمدید')");
+        $connect->query("INSERT INTO textbot (id_text,text) VALUES ('text_usertest','🔑 اکانت تست')");
+        $connect->query("INSERT INTO textbot (id_text,text) VALUES ('text_dec_usertest','$text_usertest')");
+        $connect->query("INSERT INTO textbot (id_text,text) VALUES ('text_info','📊  اطلاعات سرویس')");
+        $connect->query("INSERT INTO textbot (id_text,text) VALUES ('text_dec_info','$text_info')");
+        $connect->query("INSERT INTO textbot (id_text,text) VALUES ('text_support','☎️ پشتیبانی')");
+        $connect->query("INSERT INTO textbot (id_text,text) VALUES ('text_dec_support','$support_dec')");
+        $connect->query("INSERT INTO textbot (id_text,text) VALUES ('text_help','📚  آموزش')");
+        $connect->query("INSERT INTO textbot (id_text,text) VALUES ('text_bot_off',' ❌ ربات خاموش است ، لطفا دقایقی دیگر مراجعه کنید')");
+        $connect->query("INSERT INTO textbot (id_text,text) VALUES ('text_roll','$text_roll')");
+        $connect->query("INSERT INTO textbot (id_text,text) VALUES ('text_','$text_roll')");
+        $connect->query("INSERT INTO textbot (id_text,text) VALUES ('text_fq','❓ سوالات متداول')");
+        $connect->query("INSERT INTO textbot (id_text,text) VALUES ('text_dec_fq','$text_dec_fq')");
+
+
     }
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
@@ -84,9 +176,23 @@ try {
     if (!$table_exists) {
         $connect->query("CREATE TABLE setting (
         Bot_Status varchar(200) NOT NULL,
+        roll_Status varchar(200) NOT NULL,
         count_usertest varchar(5000) NOT NULL)");
         echo "table setting✅</br>";
-                $connect->query("INSERT INTO setting (count_usertest,Bot_Status) VALUES ('0','✅ روشن ')");
+        $active_text = "✅ روشن";
+        $connect->query("INSERT INTO setting (count_usertest,Bot_Status,roll_Status) VALUES ('0','$active_text','$active_text')");
+    } else {
+        $Check_filde = $connect->query("SHOW COLUMNS FROM setting LIKE 'Bot_Status'");
+        if (mysqli_num_rows($Check_filde) != 1) {
+            $connect->query("ALTER TABLE setting ADD Bot_Status VARCHAR(200)");
+            echo "The Bot_Status field was added ✅";
+        }
+        $Check_filde = $connect->query("SHOW COLUMNS FROM setting LIKE 'roll_Status'");
+        if (mysqli_num_rows($Check_filde) != 1) {
+            $connect->query("ALTER TABLE setting ADD roll_Status VARCHAR(200)");
+            $connect->query("UPDATE setting SET roll_Status = '✅ روشن '");
+            echo "The roll_Status field was added ✅";
+        }
     }
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
